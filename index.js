75a7b6c02561b7d36321298d466b9ffd21830ac1
yanglin <richenlin@gmail.com>
2017-11-15 11:54:23 +0800

    支持框架1.12.0

--- index.js
+++ index.js
@@ -807,6 +807,7 @@ module.exports = new Proxy({
     isUndefined: lodash.isUndefined,
     isNumber: lodash.isNumber,
     isObject: lodash.isPlainObject,
+    isRegExp: lodash.isRegExp,
     isRegexp: lodash.isRegExp,
     isSet: lodash.isSet,
     isString: lodash.isString,
